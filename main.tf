9b104d4ab87dacd72e86c6627aaf5bc09ada9765
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-09-06 11:24:31 -0400

    Update HTTP Data

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "this" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${data.http.ip.body}/32"]
+    cidr_blocks = ["${data.http.ip.response_body}/32"]
   }
 
   ingress {
